782c8b8f2d4122297757ba60c9df6e7d8ec36724
saggidinithin <saggidinithin@gmail.com>
2026-03-16 15:26:25 +0530

    Add files via upload

---
+++ biggest number.py
@@ -0,0 +1,6 @@
+a=int(input("first number:"))
+b=int(input("second number:"))
+c=int(input("third number:"))
+max=a if a>b and a>c else b if b>c else c
+print("smallest value:",min)
+print("biggest value:",max)
